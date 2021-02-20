aac7b5af01c9ed15a97283a19930b1171c7d9c78
adrien <51877882+Poto-Feu@users.noreply.github.com>
2021-02-20 11:43:27 +0100

    Set errno value

--- DynamicArray.c
+++ DynamicArray.c
@@ -17,6 +17,7 @@
     along with DynamicArrayLib. If not, see <https://www.gnu.org/licenses/>.
 */
 
+#include <errno.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,6 +26,10 @@
 
 #define DEFAULT_SIZE 15
 
+#define return_if_DA_NULL(da, rtrn_val) if(!da) { \
+	errno = EFAULT; \
+	return rtrn_val; \
+}
 
 struct DynamicArray
 {
@@ -39,7 +44,7 @@ DynamicArray *DynamicArray_init(size_t type_size)
 {
     DynamicArray *new_DA = malloc(sizeof(DynamicArray));
 
-    if(!new_DA) return NULL;
+	return_if_DA_NULL(new_DA, NULL);
 
     new_DA->type_size = type_size;
     new_DA->elements_n = 0;
@@ -54,7 +59,7 @@ DynamicArray *DynamicArray_init_and_reserve(size_t type_size, size_t element_num
 {
     DynamicArray *new_DA = malloc(sizeof(DynamicArray));
 
-    if(!new_DA) return NULL;
+	return_if_DA_NULL(new_DA, NULL);
 
     new_DA->type_size = type_size;
     new_DA->elements_n = 0;
@@ -71,16 +76,17 @@ DynamicArray *DynamicArray_init_and_reserve(size_t type_size, size_t element_num
 
 void DynamicArray_free(DynamicArray **DA)
 {
-    if(!DA) return;
-    else if((*DA)->is_alloc) free((*DA)->content);
+	return_if_DA_NULL(DA, ); /* Empty parameter since the return type is void */
+    if((*DA)->is_alloc) free((*DA)->content);
     free(*DA);
 }
 
 int DynamicArray_reserve(DynamicArray *DA, size_t element_number)
 {
-    if(!DA) return -1;
-    else if(DA->element_capacity > element_number) return 0;
-    else {
+	return_if_DA_NULL(DA, -1);
+    if(DA->element_capacity > element_number) {
+		return 0;
+	} else {
         void *temp_ptr = realloc(DA->content, element_number * DA->type_size);
 
         if(!temp_ptr) return -1;
@@ -95,8 +101,8 @@ int DynamicArray_reserve(DynamicArray *DA, size_t element_number)
 
 int DynamicArray_shrink(DynamicArray *DA, size_t element_number)
 {
-    if(!DA) return -1;
-    else if(element_number >= DA->element_capacity) {
+	return_if_DA_NULL(DA, -1);
+    if(element_number >= DA->element_capacity) {
         free(DA->content);
         DA->elements_n = 0;
         DA->content = NULL;
@@ -129,8 +135,11 @@ size_t DynamicArray_get_capacity(DynamicArray *DA)
 
 void *DynamicArray_get_element(DynamicArray *DA, size_t index)
 {
-    if(index >= DA->elements_n) return NULL;
-    else {
+	return_if_DA_NULL(DA, NULL);
+    if(index >= DA->elements_n) {
+        errno = EINVAL;
+        return NULL;
+    } else {
         char *DA_content_char = (char *)DA->content;
         return DA_content_char + (index * DA->type_size);
     }
@@ -138,6 +147,7 @@ void *DynamicArray_get_element(DynamicArray *DA, size_t index)
 
 int DynamicArray_add_element(DynamicArray *DA, void *element)
 {
+	return_if_DA_NULL(DA, -1);
     if(DA->elements_n + 1 > DA->element_capacity) {
         if(DynamicArray_reserve(DA, DA->elements_n + 1) != 0) return -1;
     }
@@ -151,6 +161,7 @@ int DynamicArray_remove_element(DynamicArray *DA, size_t pos)
 {
 	char *content = DA->content;
 
+	return_if_DA_NULL(DA, -1);
 	if(pos >= DA->elements_n) return -1;
 	memmove(content + pos * DA->type_size, content + (pos + 1) * DA->type_size,
 			DA->type_size * (DA->elements_n - pos - 1));
